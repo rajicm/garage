9f04984dd7ac92b8cfb13542a3a91f451b487b57
Maja Rajic <m.rajic@reply.de>
2024-11-25 10:25:52 +0100

    remove login component from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import AuthService from './services/auth.service';
 import { UserService } from './services/user.service';
-import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LoginComponent, RouterModule],
+  imports: [CommonModule, HeaderComponent, RouterModule],
   providers: [AuthService, UserService],
 })
 export class AppComponent {
